aa2ca2cb6420c0e88fcbd2e148a8ecb79eb1db9d
shivesh Kumar <shiveshkumarsuri@hotmail.com>
2026-05-09 20:55:42 -0500

    graph - implementation

--- main.py
+++ main.py
@@ -1,7 +1,42 @@
 from dotenv import load_dotenv
-import os
+from langchain_core.messages import HumanMessage
+from langgraph.constants import START, END
+from langgraph.graph import MessagesState, StateGraph
+from nodes import run_agent_reasoning, tool_node
 
 load_dotenv()
+
+AGENT_REASON = "agent_reason"
+ACT = "act"
+LAST = -1
+
+
+def should_continue(state: MessagesState) -> str:
+    if not state["messages"]:
+        return END
+    return ACT
+
+######Graph Implementation#####
+
+flow = StateGraph(MessagesState)
+
+#Adding Nodes
+flow.set_entry_point(AGENT_REASON)
+flow.add_node(AGENT_REASON, run_agent_reasoning)
+flow.add_node(ACT, tool_node)
+
+#Adding Edges
+flow.add_edge(START, AGENT_REASON)
+flow.add_conditional_edges(AGENT_REASON,should_continue, {
+    END:END,
+    ACT:ACT})
+flow.add_edge(ACT, AGENT_REASON)
+
+#Generating Graph
+app = flow.compile()
+app.get_graph().draw_mermaid_png(output_file_path="flow.png")
+
+
 
 def main():
     print("Hello from project-langgraph!")
